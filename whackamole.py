d2cd13f05b71ab7d6b39c0c70ff741c9c92c6204
Supreet T <supsrasri@gmail.com>
2024-11-13 18:52:48 -0500

    finished project

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,53 @@
 import pygame
+from random import randrange
 
+DISPLAY_WIDTH = 640
+DISPLAY_HEIGHT = 512
+GRID_COLS = 20
+GRID_ROWS = 16
+SQUARE_SIZE = 32
+
+def draw_lines(screen):
+    # Vertical Lines
+    for i in range(1, GRID_COLS):
+        x = (DISPLAY_WIDTH / GRID_COLS) * i
+        pygame.draw.line(screen, (13, 112, 13), (x, 0), (x, DISPLAY_HEIGHT))
+    
+    # Horizontal Lines
+    for i in range(1, GRID_ROWS):
+        y = (DISPLAY_HEIGHT / GRID_ROWS) * i
+        pygame.draw.line(screen, (13, 112, 13), (0, y), (DISPLAY_WIDTH, y))
+    
+
+def draw_mole(screen, mole_image, mole_pos):
+    screen.blit(mole_image, mole_image.get_rect(topleft=mole_pos))
+
+def gen_centered_pos():
+    col = randrange(0, GRID_COLS)
+    row = randrange(0, GRID_ROWS)
+
+    return (SQUARE_SIZE * col, SQUARE_SIZE * row )
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
         mole_image = pygame.image.load("mole.png")
-        screen = pygame.display.set_mode((640, 512))
+        (mole_width, mole_height) = mole_image.get_size()
+        mole_pos = gen_centered_pos()
+        screen = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
         clock = pygame.time.Clock()
         running = True
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    clicked_mole = pygame.Rect((*mole_pos, mole_width, mole_height)).collidepoint(*event.pos)
+                    if clicked_mole:
+                        mole_pos = gen_centered_pos()
             screen.fill("light green")
+            draw_lines(screen)
+            draw_mole(screen, mole_image, mole_pos)
             pygame.display.flip()
             clock.tick(60)
     finally:
